221d73ba8c0d1b48a82e08c67a6140edb5309b20
ArturoTorres <arturo.torres@list.lu>
2022-03-04 22:10:58 +0100

    implementation sce-ua

---
+++ calibration_test/main.py
@@ -0,0 +1,93 @@
+
+# title: "Main file for Implementation of the SCE-UA Algorithm for calibration of the run_BRIN_model"
+# author: "J. Arturo Torres-Matallana(1)"
+# organization (1): Luxembourg Institute of Science and Technology (LIST)
+# date: 12.02.2022 - 03.03.2022
+
+# import pandas as pd
+# import rpy2.robjects as robjects
+# from rpy2.robjects import pandas2ri
+# pandas2ri.activate()
+# from rpy2.robjects.packages import importr
+# xts= importr('xts', lib_loc="/usr/local/lib/R/site-library",
+#              robject_translations = {".subset.xts": "_subset_xts2",
+#                                      "to.period": "to_period2"})
+
+# import numpy as np
+# from scipy import stats
+# from vineyard import *
+
+# from spotpy.parameter import Uniform
+# from spotpy.objectivefunctions import rmse
+# import os
+# import func
+# import sce_ua
+
+import os
+
+
+
+def my_sce_ua(d_ini, id_cpu, path_data, path_obs, cv_i):
+    # importing  all the functions defined in func.py
+    from func import f_data_in, f_subset, f_data_obs, f_subset_obs, f_subset_2
+    from sce_ua import f
+
+    # Define output folder
+    folder_output = os.path.basename(path_obs)
+    folder_output = os.path.splitext(folder_output)
+    folder_output = folder_output[0]
+
+    # 'Create output folder
+    try:
+        os.mkdir(folder_output)
+    except OSError as error:
+        print(error)
+
+    # # ' Setup input data filepaths
+    # data_file = path_data
+    #
+    # # ' Reading dataset Remich
+    # # here \s+ for sep means any one or more white space character
+    # data = f_data_in(dat_file=data_file, dat_sep=',', dat_day='day', dat_month='month', dat_year='Year')
+    # # print(type(data))
+    # # print(data.head())
+    #
+    # # ' Subset data by date
+    # if len(d_ini) == 2:
+    #     data_subset = f_subset(dat=data, date_ini=d_ini[0], date_end=d_ini[1])
+    #     # print(type(data_subset))
+    #     # print(data_subset)
+    # elif len(d_ini) == 4:
+    #     data_subset = f_subset_2(dat=data, date_ini=d_ini[0], date_end=d_ini[1], date_ini_2=d_ini[2], date_end_2=d_ini[3])
+    #
+    # # ' Export data_subset as csv file
+    # data_subset.to_csv(folder_output + '/tmp_data_subset_' + str(d_ini[0]) + '_' + str(d_ini[len(d_ini) - 1]) + '.csv')
+    #
+    # # ' Plotting time series
+    # import matplotlib.pyplot as plt
+    #
+    # plt.figure(figsize=(16, 9))
+    # plt.plot(data_subset['date'], data_subset['tas'], linewidth=0.5)
+    # # plt.show()
+    #
+    # # show current monitor dpi https://www.infobyip.com/detectmonitordpi.php
+    # plt.savefig(folder_output + '/tas_' + str(d_ini[0]) + '_' + str(d_ini[len(d_ini) - 1]) + '.png', dpi=175)
+    #
+    # # Read observational data
+    # data_file_obs = path_obs
+    #
+    # obs = f_data_obs(dat_file=data_file_obs, dat_sep=',')
+    # print(obs)
+    # print(data_subset)
+    #
+    # year = data_subset['Year'].unique()  # TODO: define for generic 'jahr' i.e 'year' instead
+    #
+    # # obs_subset = f_subset_obs(dat=obs, year_ini=np.int64(d_ini[0:4]), year_end=np.int64(d_end[0:4]))
+    # obs_subset = f_subset_obs(dat=obs, year_ini=year[0], year_end=year[len(year) - 1])
+    # print(obs_subset)
+    # obs_bbch09 = obs_subset[["Years", "budburst.DOY."]]
+    # print(obs_bbch09)
+    # obs_bbch09.to_csv(folder_output + '/tmp_obs_' + str(d_ini[0]) + '_' + str(d_ini[len(d_ini) - 1]) + '.csv')
+
+
+    f(d_ini, id_cpu, folder_output, path_data, path_obs, cv_i+1)
--- calibration_test/run_calibrations.py
+++ calibration_test/run_calibrations.py
@@ -1,5 +1,10 @@
-from PyPhenoModels.Multi_phenology_model_classes import *
-import pandas as pd
+
+# title: "Run the SCE-UA Algorithm for calibration of the run_BRIN_model"
+# author: "J. Arturo Torres-Matallana(1); Chenyao Yang(2)"
+# organization (1): Luxembourg Institute of Science and Technology (LIST)
+# organization (2): ... (UTAD)
+# date: 12.02.2022 - 03.03.2022
+
 import time
 from datetime import datetime,timedelta
 ######################################################################################################################################################################
@@ -17,27 +22,28 @@ class Timer():
         self._duration()
         print(f"[BENCHMARK] End Time - {datetime.now()}")
 ######################################################################################################################################################################
-# 1. Load observed phenology data and weather data
-budburst_raw = pd.read_csv("sample_data.csv") # Here the sample data only contains the budburst date for a given variety
-budburst_ob = pd.Series(budburst_raw.iloc[:, 1].values, index = budburst_raw["Year"], name = "budburst_ob") # Obtain the series for observation
-study_years = [min(budburst_ob.index)-1] + budburst_ob.index.to_list()  # Obtain the inferred study years. Get -1 year so that it includes the preceding year of the first starting year
-weather_OB = pd.read_csv("Lisboa region_no_NAN.csv") # Load the weather station data for Lisbon
-weather_OB_datetime_index = pd.to_datetime(weather_OB.loc[:, weather_OB.columns.isin(["day", "month", "Year"])]) # Generate the datetime index
-T_min_series = pd.Series(weather_OB["tasmin"].values, index = weather_OB_datetime_index, name = "tasmin") # Obtain the time series of daily minimum temperature, the name should be named as tasmin. 
-T_max_series = pd.Series(weather_OB["tasmax"].values, index = weather_OB_datetime_index, name = "tasmax") # Obtain the time series of daily maximum temperature,  the name should be named as tasmax. 
-# Subset the series for which only study years are analyzed
-T_min_series = T_min_series.loc[T_min_series.index.year.isin(study_years)]
-T_max_series = T_max_series.loc[T_max_series.index.year.isin(study_years)]
-# 2. Run BRIN model to generate output for dormancy and budburst date
-dormancy_out, budburst_out = run_BRIN_model(T_min_series, T_max_series, CCU_dormancy=130, CGDH_budburst = 7500.0, 
-                   TMBc_budburst= 25.0, TOBc_budburst = 8.19)
-# A list of BRIN model parameters that need to be calibrated (the docst can be retrieved by calling run_BRIN_model.__doc__)
-# CCU_dormancy: float, a BRIN model parameter that represents the cumulative chilling unit to break the dormancy with calculations starting from the starting_DOY.
-# CGDH_budburst: float, a BRIN model parameter that represents the cumulative growing degree hours to reach the budbust onset from the dormancy break DOY.
-# TMBc_budburst: float, a BRIN model parameter that represents the upper temperatue threshold for the linear response function.
-# TOBc_budburst: float, a BRIN model parameter that represents the base temperature for the post-dormancy period.
 
-# 3.Compare predictions vs observations # Note here we only need to analyze the budburst DOY
-budburst_pred = pd.Series(budburst_out.apply(lambda x: x.dayofyear).values, index = budburst_out.index, name="budburst_pred") # Obtain the prediction vector for budburst
+from main import my_sce_ua
+import random
 
-# The next step should be to compare budburst_pred with budburst_ob
+time_start = datetime.now()
+time_start_str = time_start.strftime("%Y-%m-%d %H:%M:%S")
+print("Start time =", time_start_str)
+
+path_data1 = ("./calibration_test/Lisboa region_no_NAN.csv")
+
+path_obs1 = ("./calibration_test/sample_data.csv")
+
+
+d_ini = ["1990-01-01", "2014-12-31"]
+id_cpu = random.randint(100, 999)
+
+for cv_i in range(1): # run until 20
+    my_sce_ua(d_ini, id_cpu, path_data1, path_obs1, cv_i)
+
+time_end = datetime.now()
+time_end_str = time_end.strftime("%Y-%m-%d %H:%M:%S")
+print("End time =", time_end_str)
+
+time_elapsed = time_end - time_start
+print("Elapsed time =", time_elapsed)
