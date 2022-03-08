f9c103eeb3a8a048f89c40e59f15eb6673778b4a
ChenyaoYang123 <chenyao_yang@outlook.com>
2022-03-08 14:41:35 +0000

    # Adjust both model script and calibration script to enable dealing with NaN values during calibration

--- calibration_test/run_calibrations.py
+++ calibration_test/run_calibrations.py
@@ -1,6 +1,12 @@
-from PyPhenoModels.Multi_phenology_model_classes import *
-import pandas as pd
+
+# title: "Run the SCE-UA Algorithm for calibration of the run_BRIN_model"
+# author: "J. Arturo Torres-Matallana(1); Chenyao Yang(2)"
+# organization (1): Luxembourg Institute of Science and Technology (LIST)
+# organization (2): ... (UTAD)
+# date: 12.02.2022 - 03.03.2022
+
 import time
+import os
 from datetime import datetime,timedelta
 ######################################################################################################################################################################
 class Timer():
@@ -17,27 +23,30 @@ class Timer():
         self._duration()
         print(f"[BENCHMARK] End Time - {datetime.now()}")
 ######################################################################################################################################################################
-# 1. Load observed phenology data and weather data
-budburst_raw = pd.read_csv("sample_data.csv") # Here the sample data only contains the budburst date for a given variety
-budburst_ob = pd.Series(budburst_raw.iloc[:, 1].values, index = budburst_raw["Year"], name = "budburst_ob") # Obtain the series for observation
-study_years = [min(budburst_ob.index)-1] + budburst_ob.index.to_list()  # Obtain the inferred study years. Get -1 year so that it includes the preceding year of the first starting year
-weather_OB = pd.read_csv("Lisboa region_no_NAN.csv") # Load the weather station data for Lisbon
-weather_OB_datetime_index = pd.to_datetime(weather_OB.loc[:, weather_OB.columns.isin(["day", "month", "Year"])]) # Generate the datetime index
-T_min_series = pd.Series(weather_OB["tasmin"].values, index = weather_OB_datetime_index, name = "tasmin") # Obtain the time series of daily minimum temperature, the name should be named as tasmin. 
-T_max_series = pd.Series(weather_OB["tasmax"].values, index = weather_OB_datetime_index, name = "tasmax") # Obtain the time series of daily maximum temperature,  the name should be named as tasmax. 
-# Subset the series for which only study years are analyzed
-T_min_series = T_min_series.loc[T_min_series.index.year.isin(study_years)]
-T_max_series = T_max_series.loc[T_max_series.index.year.isin(study_years)]
-# 2. Run BRIN model to generate output for dormancy and budburst date
-dormancy_out, budburst_out = run_BRIN_model(T_min_series, T_max_series, CCU_dormancy=130, CGDH_budburst = 7500.0, 
-                   TMBc_budburst= 25.0, TOBc_budburst = 8.19)
-# A list of BRIN model parameters that need to be calibrated (the docst can be retrieved by calling run_BRIN_model.__doc__)
-# CCU_dormancy: float, a BRIN model parameter that represents the cumulative chilling unit to break the dormancy with calculations starting from the starting_DOY.
-# CGDH_budburst: float, a BRIN model parameter that represents the cumulative growing degree hours to reach the budbust onset from the dormancy break DOY.
-# TMBc_budburst: float, a BRIN model parameter that represents the upper temperatue threshold for the linear response function.
-# TOBc_budburst: float, a BRIN model parameter that represents the base temperature for the post-dormancy period.
 
-# 3.Compare predictions vs observations # Note here we only need to analyze the budburst DOY
-budburst_pred = pd.Series(budburst_out.apply(lambda x: x.dayofyear).values, index = budburst_out.index, name="budburst_pred") # Obtain the prediction vector for budburst
+from main import my_sce_ua
+import random
 
-# The next step should be to compare budburst_pred with budburst_ob
+time_start = datetime.now()
+time_start_str = time_start.strftime("%Y-%m-%d %H:%M:%S")
+print("Start time =", time_start_str)
+
+test_main_dir = r"J:\Study6_Multi_phenology_modelling_seasonal_forecast"
+os.chdir(test_main_dir) # Change to the current directory  
+path_obs1 = os.path.join(test_main_dir, "sample_data.csv")  #("./calibration_test/sample_data.csv")
+path_data1 = os.path.join(test_main_dir, "Lisboa_region_no_NAN.csv") # ("./calibration_test/Lisboa region_no_NAN.csv")
+
+
+d_ini = ["1990-01-01", "2014-12-31"]
+id_cpu = random.randint(100, 999)
+cv_i = 10
+#for cv_i in range(1): # run until 20
+# Start optimization of parameters
+my_sce_ua(d_ini, id_cpu, path_data1, path_obs1, cv_i)
+
+time_end = datetime.now()
+time_end_str = time_end.strftime("%Y-%m-%d %H:%M:%S")
+print("End time =", time_end_str)
+
+time_elapsed = time_end - time_start
+print("Elapsed time =", time_elapsed)
